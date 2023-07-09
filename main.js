fcaf16884a0ebb69759881c033ea06d375ffa644
wtrdr-ams-2023 <139046410+wtrdr-ams-2023@users.noreply.github.com>
2023-07-09 20:02:57 +0900

    修正

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-const taskValue = document.getElementsByClassName('task_value')[0];
-const taskSubmit = document.getElementsByClassName('task_submit')[0];
-const taskList = document.getElementsByClassName('task_list')[0];
+let taskValue = document.getElementsByClassName('task_value')[0];
+let taskSubmit = document.getElementsByClassName('task_submit')[0];
+let taskList = document.getElementsByClassName('task_list')[0];
 
 // 追加ボタンを作成
-const addTasks = (task) => {
+let addTasks = (task) => {
   // 入力したタスクを追加・表示
-  const listItem = document.createElement('li');
-  const showItem = taskList.appendChild(listItem);
+  let listItem = document.createElement('li');
+  let showItem = taskList.appendChild(listItem);
   showItem.innerHTML = task;
 
   // タスクに削除ボタンを付与
-  const deleteButton = document.createElement('button');
+  let deleteButton = document.createElement('button');
   deleteButton.innerHTML = 'Delete';
   listItem.appendChild(deleteButton);
 
@@ -22,15 +22,15 @@ const addTasks = (task) => {
 };
 
 // 削除ボタンにタスクを消す機能を付与
-const deleteTasks = (deleteButton) => {
-  const chosenTask = deleteButton.closest('li');
+let deleteTasks = (deleteButton) => {
+  let chosenTask = deleteButton.closest('li');
   taskList.removeChild(chosenTask);
 };
 
 // 追加ボタンをクリックし、イベントを発動（タスクが追加）
 taskSubmit.addEventListener('click', evt => {
   evt.preventDefault();
-  const task = taskValue.value;
+  let task = taskValue.value;
   addTasks(task);
   taskValue.value = '';
 });
